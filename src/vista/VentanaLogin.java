4fec85ea030ee711f4afa48143b1083c3f9e43fe
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-04-24 14:13:57 -0500

    Se termino de hacer la ventana login

--- src/vista/VentanaLogin.java
+++ src/vista/VentanaLogin.java
@@ -1,5 +1,7 @@
 package vista;
 
+import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,12 +11,12 @@ public class VentanaLogin extends JFrame implements ActionListener{
 	
 	ImageIcon iconoUsuario=new ImageIcon("./recursos/usuario.png");
 	
-	JLabel lbl1 = new JLabel(iconoUsuario);
+	JLabel lbl1 = new JLabel();
 	
 	private JTextField txtIdUsuario=new JTextField(10);
 	private JTextField txtContraseña=new JTextField(10);
 	
-	JButton btnIngresar=new JButton();
+	JButton btnIngresar=new JButton("Ingresar");
 	
 	
 	public VentanaLogin() {
@@ -22,12 +24,30 @@ public class VentanaLogin extends JFrame implements ActionListener{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(300, 400);
 		setVisible(true);
+		this.getContentPane().setBackground(new Color(147, 192, 244));
 		setTitle("Iniciar Secion");
 		
+		//Icono
 		lbl1.setBounds(100,0,100,100);
-		lbl1.setIcon(iconoUsuario);
+		lbl1.setIcon(new ImageIcon(iconoUsuario.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
 		add(lbl1);
 		
+		JLabel lb2=new JLabel("ID. usuario");
+		lb2.setBounds(120, 110, 100, 25);
+		add(lb2);
+		
+		txtIdUsuario.setBounds(80, 130, 150, 20);
+		add(txtIdUsuario);
+		
+		JLabel lb3=new JLabel("Contraeña");
+		lb3.setBounds(120, 150, 76, 25);
+		add(lb3);
+		
+		txtContraseña.setBounds(80, 175, 150, 20);
+		add(txtContraseña);
+		
+		btnIngresar.setBounds(80, 215, 150, 30);
+		add(btnIngresar);
 	}
 	
 	@Override
