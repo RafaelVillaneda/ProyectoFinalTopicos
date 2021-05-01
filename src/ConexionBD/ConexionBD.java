44869f76531c92be1ed1bf008aa99434e0d23bf0
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-05-01 15:28:23 -0500

    Se creo la clase de ConexionBD

---
+++ src/ConexionBD/ConexionBD.java
@@ -0,0 +1,75 @@
+package ConexionBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConexionBD {
+	
+	private Connection conexion;
+	private Statement stm;
+	private ResultSet rs;
+	
+	public ConexionBD() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String URL="jdbc:mysql://localhost:3306/libreria";
+			
+			conexion=DriverManager.getConnection(URL,"root","REVT2001");
+		
+			System.out.println("CONEXION ESTABLECIDA");
+			System.out.println("Ya casi soy Ingeniero inmortal");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("ERROR DE DRIVER");
+		}catch (SQLException e) {
+			System.out.println("Error de conexion a mysql o de la base de datos");
+		}
+	}
+	
+	public void cerrarConexion() {
+		try {
+			stm.close();
+			conexion.close();
+		} catch (SQLException e) {
+			System.out.println("Error de cierre concexion");
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	public static void main(String[] args) {
+		new ConexionBD();
+	}
+	*/
+	//-----------------------METODO PARA OPERCIONES DDL DML (ABC)
+	public boolean ejecutarInstruccion(String sql) {
+		try {
+			stm= conexion.createStatement();
+			int resultado=stm.executeUpdate(sql); //1 todobien 2 todo mal
+			return resultado==1?true:false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	//------------------------METODO PARA OPERACIONES DE CONSULTAS
+	public ResultSet ejecutarConsulta(String sql) {
+		try {
+			stm=conexion.createStatement();
+			rs=stm.executeQuery(sql);
+		} catch (SQLException e) {
+			System.out.println("No se pudo crear la");
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+}
+
+
--- src/PruebaProyecto.java
+++ src/PruebaProyecto.java
@@ -1,5 +1,6 @@
 import javax.swing.SwingUtilities;
 
+import ConexionBD.*;
 import vista.VentanaCargaDescargaLibros;
 import vista.VentanaInicio;
 import vista.VentanaLogin;
@@ -10,12 +11,13 @@ public class PruebaProyecto {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			 
+			ConexionBD prueba=new ConexionBD();
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				
 				//new VentanaInicio();
-				new VentanaLogin();
+				//new VentanaLogin();
 			}
 		});
 
