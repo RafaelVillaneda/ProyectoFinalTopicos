20a9ebde1ae454f6459010ccbbcc3112bb735540
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-05-01 15:19:58 -0500

    Se agrego la calse de libro con sus resectivos atributos encapsulados

---
+++ src/modelo/libro.java
@@ -0,0 +1,44 @@
+package modelo;
+
+public class libro {
+
+	private String nombre;
+	private String genero;
+	private String autor;
+	private String editorial;
+	public libro(String nombre, String genero, String autor, String editorial) {
+		super();
+		this.nombre = nombre;
+		this.genero = genero;
+		this.autor = autor;
+		this.editorial = editorial;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getEditorial() {
+		return editorial;
+	}
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	
+	
+	
+}
