65ace311f3b9cdd1c9e89c47e153186e8377ac08
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-05-08 13:24:18 -0500

    Se acabo de configurar la ventana de consultas libros

--- src/PruebaProyecto.java
+++ src/PruebaProyecto.java
@@ -9,6 +9,7 @@ import vista.VentanaLogin;
 public class PruebaProyecto {
 
 	public static void main(String[] args) {
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			 
 			//ConexionBD prueba=new ConexionBD();
--- src/vista/VentanaConsultasLibros.java
+++ src/vista/VentanaConsultasLibros.java
@@ -1,7 +1,13 @@
 package vista;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.sql.SQLException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -10,10 +16,11 @@ import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
-public class VentanaConsultasLibros extends JInternalFrame{
+public class VentanaConsultasLibros extends JInternalFrame implements ActionListener{
 	
 	JTextField cajatitulo,cajaAutor,cajaEditorial,cajaGeneros,cajaId;
 	JButton btnBuscar,btnLimpiar,btnRegresar;
@@ -73,11 +80,87 @@ public class VentanaConsultasLibros extends JInternalFrame{
 		lb1=new JLabel("---------------------------------------------------------------------------------------");
 		lb1.setBounds(0, 145, 350, 50);add(lb1);
 		
-		JScrollPane scroll=new JScrollPane(tablaLibros);
-		scroll.setBounds(10, 180, 325, 100);add(scroll);
+		btnRegresar=new JButton(iconoRegresar);
+		btnRegresar.setIcon(new ImageIcon(iconoRegresar.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
+		btnRegresar.setBounds(10, 180, 50, 50);add(btnRegresar);
 		
+		btnLimpiar=new JButton(iconoLimpiar);
+		btnLimpiar.setIcon(new ImageIcon(iconoLimpiar.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
+		btnLimpiar.setBounds(275, 180, 50, 50);add(btnLimpiar);
+		
+		
+		actualizarTabla();
+		JScrollPane scroll=new JScrollPane(tablaLibros);
+		scroll.setBounds(10, 250, 325, 100);add(scroll);
+		
+		//VALIDACION
+		cajaId.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char car = e.getKeyChar();
+				if(Character.isDigit(car)){
+					//String cadena=txtNombre.getText();
+					//txtNumControl.setText(cadena+e);
+				}else{
+				e.consume();
+				getToolkit().beep();
+				}
+			}
+			@Override public void keyPressed(KeyEvent e) {}
+			@Override public void keyReleased(KeyEvent e) {}
+		});
+		
+		btnBuscar.addActionListener(this);
+		btnLimpiar.addActionListener(this);
+		btnRegresar.addActionListener(this);
+	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnBuscar) {
+			//Busqueda
+		}else if(e.getSource()==btnLimpiar) {
+			restablecer(cajaAutor,cajaEditorial,cajaGeneros,cajaId,cajatitulo);
+		}else if(e.getSource()==btnRegresar) {
+			setVisible(false);
+		}
 		
 	}
+	public void actualizarTabla() {
+		
+		String controlador = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/libreria";
+		String consulta = "SELECT * FROM libros";
+		
+		ResultSetTableModel modeloDatos=null;
+		try {
+			modeloDatos = new ResultSetTableModel(controlador, url, consulta);
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		tablaLibros.setModel(modeloDatos);
+		//scroll.setPreferredSize( 400, 600 );
+		
+	}
+	public void restablecer(Component...ComonentesGraficos){
+		
+		for (Component Component : ComonentesGraficos) {
+			if(Component instanceof JComboBox) {
+				((JComboBox<?>)Component).setSelectedIndex(0);
+			}else if(Component instanceof JTextField) {
+				((JTextField)Component).setText("");
+			}else if(Component instanceof JSpinner) {
+				((JSpinner)Component).setValue(0);;
+			}
+		}
+		
+	}//Restablecer
+	
 	
 	
 	
